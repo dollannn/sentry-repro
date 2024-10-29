bedede7360ae2720768aacc6b54178370f9be247
Dollan <dollan@duck.com>
2024-10-30 00:52:25 +0100

    Add sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,19 @@
+import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sentrySvelteKit({
+			sourceMapsUploadOptions: {
+				org: 'playpark',
+				project: 'sentry-repro',
+				url: 'https://sentry.playpark.se/',
+				sourcemaps: {
+					filesToDeleteAfterUpload: '**/*.js.map',
+				},
+			},
+		}),
+		sveltekit(),
+	],
 });
